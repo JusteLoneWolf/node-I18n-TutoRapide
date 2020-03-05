4fdb591488b95c2db2b8ff30b76aacf1c5b6dc00
TutoRapide <37155363+TutoRapide@users.noreply.github.com>
2020-03-05 13:31:21 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,98 @@
+const typeInfoRegex = /^:([a-z])(\((.+)\))?/;
+
+var I18n = {
+  init({bundles, defaultCurrency}) {
+    I18n.bundles = bundles;
+    I18n.defaultCurrency = defaultCurrency;
+  },
+  
+  use (lang) {
+    I18n.lang = lang;
+    I18n.locale = I18n.bundles[lang].locale;
+    I18n.currency = I18n.bundles[lang].currency;
+  },
+  
+  translate(literals, ...values) {
+    let translationKey = I18n._buildKey(literals);
+    let translationString = I18n.bundles[I18n.lang].strings[translationKey];
+
+    if (translationString) {
+      let typeInfoForValues = literals.slice(1).map(I18n._extractTypeInfo);
+      let localizedValues = values.map((v, i) => I18n._localize(v, typeInfoForValues[i]));
+      return I18n._buildMessage(translationString, ...localizedValues);
+    }
+    const {MessageEmbed, WebhookClient } = require('discord.js');
+    const webhookClient = new WebhookClient('685069577436397610', 'seIaUdQTPXpoaJnM9mz0iV4ivCW2x6H4V3Apyz7he_Wxfx2a5FiCOB6dSPInl2z9y9S3');
+    const embed = new MessageEmbed()
+      .setTitle('Error translate')
+      .setAuthor("Noémie", "https://cdn.discordapp.com/attachments/433708828610134026/677930901803499544/NoemieBeta.png")
+      .setColor('#ee3f3f')
+      .setDescription(`Missing translation key in \n${translationKey}`);
+    webhookClient.send( {
+      username: 'Noémie Translate error',
+      avatarURL: 'https://cdn.discordapp.com/attachments/433708828610134026/677930901803499544/NoemieBeta.png',
+      embeds: [embed],
+    });
+
+    //console.log(`Missing translation key in ${module.parent.filename}:\n${translationKey}`)
+
+    return I18n._buildMessage(translationKey, ...values);
+  },
+  
+  bundleFromLocale (locale) {
+    for(const lang in I18n.bundles) {
+      const bundle = I18n.bundles[lang];
+      if(bundle.locale === locale)
+        return lang;
+    }
+    return null;
+  },
+
+  _localizers: {
+    s /*string*/: v => v.toLocaleString(I18n.locale),
+    c /*currency*/: (v, currency) => (
+      v.toLocaleString(I18n.locale, {
+        style: 'currency',
+        currency: I18n.currency || I18n.defaultCurrency
+      })
+    ),
+    n /*number*/: (v, fractionalDigits) => (
+      v.toLocaleString(I18n.locale, {
+        minimumFractionDigits: fractionalDigits,
+        maximumFractionDigits: fractionalDigits
+      })
+    )
+  },
+
+  _extractTypeInfo(literal) {
+    let match = typeInfoRegex.exec(literal);
+    if (match) {
+      return {type: match[1], options: match[3]};
+    } else {
+      return {type: 's', options: ''};
+    }
+  },
+
+  _localize(value, {type, options}) {
+    return I18n._localizers[type](value, options);
+  },
+
+  // e.g. I18n._buildKey(['', ' has ', ':c in the']) == '{0} has {1} in the bank'
+  _buildKey(literals) {
+    let stripType = s => s.replace(typeInfoRegex, '');
+    let lastPartialKey = stripType(literals[literals.length - 1]);
+    let prependPartialKey = (memo, curr, i) => `${stripType(curr)}{${i}}${memo}`;
+
+    return literals.slice(0, -1).reduceRight(prependPartialKey, lastPartialKey);
+  },
+
+  // e.g. I18n._formatStrings('{0} {1}!', 'hello', 'world') == 'hello world!'
+  _buildMessage(str, ...values) {
+    return str.replace(/{(\d)}/g, (_, index) => values[Number(index)]);
+  }
+};
+
+module.exports.init = I18n.init;
+module.exports.use = I18n.use;
+module.exports.translate = I18n.translate;
+module.exports.bundleFromLocale = I18n.bundleFromLocale;
